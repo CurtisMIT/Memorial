997bf6dfcf5fcae3fb54a47a88c38b47181d8412
Elaine Lau <elaine@Elaines-MacBook-Pro.local>
2020-02-01 15:07:32 -0500

    Name,Date,Dead Done

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
-
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
-print(os.environ['APP_SETTINGS'])
+# print(os.environ['APP_SETTINGS'])
 
 from models import Person
 
@@ -16,9 +14,64 @@ from models import Person
 def hello():
     return "Hello World!"
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route("/add")
+def add_person():
+    name=request.args.get('name')
+    birth=request.args.get('birth')
+    dead=request.args.get('dead')
+    try:
+        person=Person(
+            name=name,
+            birth=birth,
+            dead=dead
+        )
+        db.session.add(person)
+        db.session.commit()
+        return "person added. person id={}".format(person.id)
+    except Exception as e:
+	    return(str(e))
+#http://127.0.0.1:5000/add?name=Twilight&birth=Stephenie Meyer&dead=2006
+
+#returns a list of all deceased 
+@app.route("/getall")
+def get_all():
+    try:
+        people=Person.query.all()
+        return jsonify([e.serialize() for e in people])
+    except Exception as e:
+	    return(str(e))
+
+#returns the deceased info based on given id 
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        person=Person.query.filter_by(id=id_).first()
+        return jsonify(person.serialize())
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/add/form", methods=['GET', 'POST'])
+def add_person_form():
+    if request.method == 'POST':
+        name    =request.form.get('name')
+        birth   =request.form.get('bith')
+        dead    =request.form.get('dead')
+        try:
+            person=Person(
+                name    =name,
+                birth   =birth,
+                dead    =dead
+            )
+            db.session.add(person)
+            db.session.commit()
+            return "Person added. person id={}".format(person.id)
+        except Exception as e:
+            return(str(e))
+    return render_template("getdata.html")
+    
+# @app.route('/<name>')
+# def hello_name(name):
+#     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -3,11 +3,15 @@ from app import db
 class Person(db.Model):
     __tablename__ = 'people'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String())
+    id      = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String())
+    birth   = db.Column(db.String())
+    dead    = db.Column(db.String())
 
-    def __init__(self, name):
+    def __init__(self, name, birth, dead):
         self.name = name
+        self.birth = birth 
+        self.dead = dead 
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -15,5 +19,7 @@ class Person(db.Model):
     def serialize(self):
         return {
             'id': self.id,
-            'name': self.name
+            'name': self.name,
+            'birth': self.birth,
+            'dead': self.dead
         }
